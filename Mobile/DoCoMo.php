2e35797753c56dfe37034bb62c6b89b51a0d0625
KUBO Atsuhiro <kubo@iteman.jp>
2003-04-22 08:49:39 +0000

    - getHTMLVersion(): Added '661i'.

--- Mobile/DoCoMo.php
+++ Mobile/DoCoMo.php
@@ -16,7 +16,7 @@
 // | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
 // +----------------------------------------------------------------------+
 //
-// $Id: DoCoMo.php,v 1.6 2003/04/06 08:36:29 kuboa Exp $
+// $Id: DoCoMo.php,v 1.7 2003/04/22 08:49:39 kuboa Exp $
 //
 
 require_once('Net/UserAgent/Mobile/Common.php');
@@ -68,7 +68,7 @@ require_once('Net/UserAgent/Mobile/DoCoMoDisplayMap.php');
  * @category Networking
  * @author   KUBO Atsuhiro <kubo@isite.co.jp>
  * @access   public
- * @version  $Revision: 1.6 $
+ * @version  $Revision: 1.7 $
  * @see      Net_UserAgent_Mobile_Common
  * @link     http://www.nttdocomo.co.jp/p_s/imode/spec/useragent.html
  * @link     http://www.nttdocomo.co.jp/p_s/imode/tag/imodetag.html
@@ -168,18 +168,24 @@ class Net_UserAgent_Mobile_DoCoMo extends Net_UserAgent_Mobile_Common
             && preg_match('/^\((.*)\)$/', $foma_or_comment, $matches)
             ) {
 
-            // DoCoMo/1.0/P209is (Google CHTML Proxy/1.0)
+            /**
+             * DoCoMo/1.0/P209is (Google CHTML Proxy/1.0)
+             */
             $this->_comment = $matches[1];
             $result = $this->_parseMain($main);
         } elseif ($foma_or_comment) {
 
-            // DoCoMo/2.0 N2001(c10;ser0123456789abcde;icc01234567890123456789)
+            /**
+             * DoCoMo/2.0 N2001(c10;ser0123456789abcde;icc01234567890123456789)
+             */
             $this->_is_foma = true;
             list($this->name, $this->version) = explode('/', $main);
             $result = $this->_parseFOMA($foma_or_comment);
         } else {
 
-            // DoCoMo/1.0/R692i/c10
+            /**
+             * DoCoMo/1.0/R692i/c10
+             */
             $result = $this->_parseMain($main);
         }
 
@@ -220,14 +226,11 @@ class Net_UserAgent_Mobile_DoCoMo extends Net_UserAgent_Mobile_Common
                                                      'html_version_map'
                                                      );
         if ($html_version_map === null) {
-
-            // http://www.nttdocomo.co.jp/p_s/imode/spec/useragent.html
             $html_version_map = array(
-                                      // regex => version
                                       '[DFNP]501i' => '1.0',
                                       '502i|821i|209i|691i|(F|N|P|KO)210i|^F671i$' => '2.0',
                                       '(D210i|SO210i)|503i|211i|SH251i|692i|200[12]|2101V' => '3.0',
-                                      '504i|251i|^F671iS$|212i|2051|2102V' => '4.0',
+                                      '504i|251i|^F671iS$|212i|2051|2102V|661i' => '4.0',
                                       'eggy|P751v' => '3.2'
                                       );
         }
