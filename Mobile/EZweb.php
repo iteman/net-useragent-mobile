8da8c22b21e62b7b177e34c473ab81a60d658ebb
KUBO Atsuhiro <kubo@iteman.jp>
2004-09-25 11:02:37 +0000

    - Implemented getCarrierShortName() and getCarrierLongName() methods. - Added isTUKa() method. - Changed treatment of the name of the model. getModel() method   currently returns the device ID.

--- Mobile/EZweb.php
+++ Mobile/EZweb.php
@@ -16,7 +16,7 @@
 // | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
 // +----------------------------------------------------------------------+
 //
-// $Id: EZweb.php,v 1.12 2004/05/27 07:24:56 kuboa Exp $
+// $Id: EZweb.php,v 1.13 2004/09/25 11:02:37 kuboa Exp $
 //
 
 require_once(dirname(__FILE__) . '/Common.php');
@@ -54,7 +54,7 @@ require_once(dirname(__FILE__) . '/Display.php');
  * @category Networking
  * @author   KUBO Atsuhiro <kubo@isite.co.jp>
  * @access   public
- * @version  $Revision: 1.12 $
+ * @version  $Revision: 1.13 $
  * @see      Net_UserAgent_Mobile_Common
  * @link     http://www.au.kddi.com/ezfactory/tec/spec/4_4.html
  * @link     http://www.au.kddi.com/ezfactory/tec/spec/new_win/ezkishu.html
@@ -118,6 +118,30 @@ class Net_UserAgent_Mobile_EZweb extends Net_UserAgent_Mobile_Common
     }
 
     // }}}
+    // {{{ isTUKa()
+
+    /**
+     * returns true if the agent is TU-Ka
+     *
+     * @return boolean
+     */
+    function isTUKa()
+    {
+        $tuka = substr($this->_deviceID, 2, 1);
+        if ($this->isWAP2()) {
+            if ($tuka == 'U') {
+                return true;
+            }
+        } else {
+            if ($tuka == 'T') {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    // }}}
     // {{{ parse()
 
     /**
@@ -148,6 +172,8 @@ class Net_UserAgent_Mobile_EZweb extends Net_UserAgent_Mobile_Common
                     preg_replace('/^\((.*)\)$/', '$1', $comment);
             }
         }
+
+        $this->_model = $this->_deviceID;
     }
 
     // }}}
@@ -182,138 +208,13 @@ class Net_UserAgent_Mobile_EZweb extends Net_UserAgent_Mobile_Common
     // {{{ getModel()
 
     /**
-     * returns name of the model like 'A5501T'
+     * returns name of the model (device ID) like 'TS21'
      *
      * @return string
      */
     function getModel()
     {
-        static $deviceIdToModel;
-        if (!isset($deviceIdToModel)) {
-            $deviceIdToModel = array(
-                                        // W (excluding W01K)
-                                        'HI32' => 'W21H',
-                                        'KC31' => 'W11K',
-                                        'HI31' => 'W11H',
-
-                                        // INFOBAR, A5000/C5000
-                                        'ST22' => 'INFOBAR',
-                                        'TS27' => 'A5504T',
-                                        'SA26' => 'A5503SA',
-                                        'KC25' => 'A5502K',
-                                        'KC24' => 'A5502K',
-                                        'TS26' => 'A5501T',
-                                        'ST23' => 'A5405SA',
-                                        'SN25' => 'A5404S',
-                                        'CA24' => 'A5403CA',
-                                        'SN24' => 'A5402S',
-                                        'CA23' => 'A5401CA', // including A5401CA II
-                                        'ST21' => 'A5306ST',
-                                        'KC22' => 'A5305K',
-                                        'TS24' => 'A5304T',
-                                        'HI24' => 'A5303H II',
-                                        'HI23' => 'A5303H',
-                                        'CA22' => 'A5302CA',
-                                        'TS23' => 'A5301T',
-                                        'TS21' => 'C5001T',
-
-                                        // A1400/A1300/A1100
-                                        'SN26' => 'A1402S',
-                                        'KC23' => 'A1401K',
-                                        'TS25' => 'A1304T', // including A1304T II
-                                        'SA25' => 'A1303SA',
-                                        'SA24' => 'A1302SA',
-                                        'SN23' => 'A1301S',
-                                        'SN22' => 'A1101S',
-
-                                        // A3000/C3000
-                                        'SA22' => 'A3015SA',
-                                        'SN21' => 'A3014S',
-                                        'TS22' => 'A3013T',
-                                        'CA21' => 'A3012CA',
-                                        'SA21' => 'A3011SA',
-                                        'MA21' => 'C3003P',
-                                        'KC21' => 'C3002K',
-                                        'HI21' => 'C3001H',
-
-                                        // A1000/C1000/C400/C300/C200
-                                        'ST14' => 'A1014ST',
-                                        'KC15' => 'A1013K',
-                                        'KC14' => 'A1012K',
-                                        'ST13' => 'A1011ST',
-                                        'SN17' => 'C1002S',
-                                        'SY15' => 'C1001SA',
-                                        'CA14' => 'C452CA',
-                                        'HI14' => 'C451H',
-                                        'TS14' => 'C415T',
-                                        'KC13' => 'C414K',
-                                        'SN15' => 'C413S',
-                                        'SN16' => 'C413S',
-                                        'SY14' => 'C412SA',
-                                        'ST12' => 'C411ST',
-                                        'TS13' => 'C410T',
-                                        'CA13' => 'C409CA',
-                                        'MA13' => 'C408P',
-                                        'HI13' => 'C407H',
-                                        'SN13' => 'C406S',
-                                        'SY13' => 'C405SA',
-                                        'SN12' => 'C404S',
-                                        'SN14' => 'C404S',
-                                        'ST11' => 'C403ST',
-                                        'DN11' => 'C402DE',
-                                        'SY12' => 'C401SA',
-                                        'KC12' => 'C313K',
-                                        'CA12' => 'C311CA',
-                                        'TS12' => 'C310T',
-                                        'HI12' => 'C309H',
-                                        'MA11' => 'C308P',
-                                        'MA12' => 'C308P',
-                                        'KC11' => 'C307K',
-                                        'SN11' => 'C305S',
-                                        'SY11' => 'C304SA',
-                                        'CA11' => 'C303CA',
-                                        'HI11' => 'C302H',
-                                        'TS11' => 'C301T',
-                                        'DN01' => 'C202DE',
-                                        'HI01' => 'C201H',
-                                        'HI02' => 'C201H',
-
-                                        // Tu-Ka
-                                        'KCU1' => 'TK41',
-                                        'SYT5' => 'TS41',
-                                        'KCTD' => 'TK40',
-                                        'TST8' => 'TT32',
-                                        'TST7' => 'TT31',
-                                        'KCTC' => 'TK31',
-                                        'SYT4' => 'TS31',
-                                        'KCTB' => 'TK23',
-                                        'KCTA' => 'TK22',
-                                        'TST6' => 'TT22',
-                                        'KCT9' => 'TK21',
-                                        'TST5' => 'TT21',
-                                        'TST4' => 'TT11',
-                                        'KCT8' => 'TK12',
-                                        'SYT3' => 'TS11',
-                                        'KCT7' => 'TK11',
-                                        'MIT1' => 'TD11',
-                                        'MAT3' => 'TP11',
-                                        'KCT6' => 'TK05',
-                                        'TST3' => 'TT03',
-                                        'KCT5' => 'TK04',
-                                        'KCT4' => 'TK03',
-                                        'SYT2' => 'TS02',
-                                        'MAT1' => 'TP01',
-                                        'MAT2' => 'TP01',
-                                        'TST2' => 'TT02',
-                                        'KCT2' => 'TK02',
-                                        'KCT3' => 'TK02',
-                                        'KCT1' => 'TK01',
-                                        'TST1' => 'TT01',
-                                        'SYT1' => 'TS01'
-                                        );
-        }
-
-        return @$deviceIdToModel[$this->_deviceID];
+        return $this->_model;
     }
 
     // }}}
@@ -366,6 +267,32 @@ class Net_UserAgent_Mobile_EZweb extends Net_UserAgent_Mobile_Common
     function isXHTMLCompliant()
     {
         return $this->_xhtmlCompliant;
+    }
+
+    // }}}
+    // {{{ getCarrierShortName()
+
+    /**
+     * returns the short name of the carrier
+     *
+     * @return string
+     */
+    function getCarrierShortName()
+    {
+        return 'E';
+    }
+
+    // }}}
+    // {{{ getCarrierLongName()
+
+    /**
+     * returns the long name of the carrier
+     *
+     * @return string
+     */
+    function getCarrierLongName()
+    {
+        return 'EZweb';
     }
 
     /**#@-*/
