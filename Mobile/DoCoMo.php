9b795c13e0c8426d5a47735e3dedeacd630c38ed
KUBO Atsuhiro <kubo@iteman.jp>
2005-03-23 08:40:17 +0000

    - Added support for 700i.   (by Christopher Kobayashi <chriskk at gmail dot com>)

--- Mobile/DoCoMo.php
+++ Mobile/DoCoMo.php
@@ -16,7 +16,7 @@
 // | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
 // +----------------------------------------------------------------------+
 //
-// $Id: DoCoMo.php,v 1.21 2005/02/01 07:07:01 kuboa Exp $
+// $Id: DoCoMo.php,v 1.22 2005/03/23 08:40:17 kuboa Exp $
 //
 
 require_once(dirname(__FILE__) . '/Common.php');
@@ -71,7 +71,7 @@ require_once(dirname(__FILE__) . '/DoCoMoDisplayMap.php');
  * @category Networking
  * @author   KUBO Atsuhiro <kubo@isite.co.jp>
  * @access   public
- * @version  $Revision: 1.21 $
+ * @version  $Revision: 1.22 $
  * @see      Net_UserAgent_Mobile_Common
  * @link     http://www.nttdocomo.co.jp/p_s/imode/spec/useragent.html
  * @link     http://www.nttdocomo.co.jp/p_s/imode/tag/imodetag.html
@@ -238,7 +238,7 @@ class Net_UserAgent_Mobile_DoCoMo extends Net_UserAgent_Mobile_Common
                                     '(D210i|SO210i)|503i|211i|SH251i|692i|200[12]|2101V' => '3.0',
                                     '504i|251i|^F671iS$|212i|2051|2102V|661i|2701|672i|(SO213i|SO213iS)' => '4.0',
                                     'eggy|P751v' => '3.2',
-                                    '505i|252i|900i|506i|880i|253i|P213i|901i' => '5.0'
+                                    '505i|252i|900i|506i|880i|253i|P213i|901i|700i' => '5.0'
                                     );
         }
 
