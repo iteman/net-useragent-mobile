d82ecf6f24248ee950d43220ba94bacfca89126d
KUBO Atsuhiro <kubo@iteman.jp>
2006-02-01 05:54:09 +0000

    - Added support for 701i and 902i in getHTMLVersion.

--- Mobile/DoCoMo.php
+++ Mobile/DoCoMo.php
@@ -16,7 +16,7 @@
 // | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
 // +----------------------------------------------------------------------+
 //
-// $Id: DoCoMo.php,v 1.24 2005/08/18 07:18:04 kuboa Exp $
+// $Id: DoCoMo.php,v 1.25 2006/02/01 05:54:09 kuboa Exp $
 //
 
 require_once(dirname(__FILE__) . '/Common.php');
@@ -71,7 +71,7 @@ require_once(dirname(__FILE__) . '/DoCoMoDisplayMap.php');
  * @category Networking
  * @author   KUBO Atsuhiro <kubo@isite.co.jp>
  * @access   public
- * @version  $Revision: 1.24 $
+ * @version  $Revision: 1.25 $
  * @see      Net_UserAgent_Mobile_Common
  * @link     http://www.nttdocomo.co.jp/p_s/imode/spec/useragent.html
  * @link     http://www.nttdocomo.co.jp/p_s/imode/tag/imodetag.html
@@ -236,9 +236,10 @@ class Net_UserAgent_Mobile_DoCoMo extends Net_UserAgent_Mobile_Common
                                     '[DFNP]501i' => '1.0',
                                     '502i|821i|209i|651|691i|(F|N|P|KO)210i|^F671i$' => '2.0',
                                     '(D210i|SO210i)|503i|211i|SH251i|692i|200[12]|2101V' => '3.0',
-                                    '504i|251i|^F671iS$|212i|2051|2102V|661i|2701|672i|(SO213i|SO213iS)' => '4.0',
+                                    '504i|251i|^F671iS$|212i|2051|2102V|661i|2701|672i|SO213i' => '4.0',
                                     'eggy|P751v' => '3.2',
-                                    '505i|252i|900i|506i|880i|253i|P213i|901i|700i|851i' => '5.0'
+                                    '505i|252i|900i|506i|880i|253i|P213i|901i|700i|851i|701i' => '5.0',
+                                    '902i' => '6.0'
                                     );
         }
 
