f9640ce3e24cce86d0f0c6cfac45c646a7584e4e
KUBO Atsuhiro <kubo@iteman.jp>
2006-09-25 02:57:50 +0000

    - Added support for SoftBank. - Changed DocBlock comments.

--- Mobile.php
+++ Mobile.php
@@ -1,41 +1,49 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
-// +----------------------------------------------------------------------+
-//
-// $Id: Mobile.php,v 1.22 2006/01/20 07:33:12 kuboa Exp $
-//
-
-require_once('PEAR.php');
-require_once(dirname(__FILE__) . '/Mobile/Request.php');
 
 /**
- * constants for error handling
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <kubo@isite.co.jp>
+ * @copyright  2003-2006 KUBO Atsuhiro <kubo@isite.co.jp>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id: Mobile.php,v 1.23 2006/09/25 02:57:50 kuboa Exp $
+ * @since      File available since Release 0.1
+ */
+
+require_once 'PEAR.php';
+require_once dirname(__FILE__) . '/Mobile/Request.php';
+
+// {{{ constants
+
+/**
+ * Constants for error handling.
  */
 define('NET_USERAGENT_MOBILE_OK',               1);
 define('NET_USERAGENT_MOBILE_ERROR',           -1);
 define('NET_USERAGENT_MOBILE_ERROR_NOMATCH',   -2);
 define('NET_USERAGENT_MOBILE_ERROR_NOT_FOUND', -3);
 
+// }}}
+// {{{ GLOBALS
+
 /**
  * globals for fallback on no match
- * @name $_NET_USERAGENT_MOBILE_FALLBACK_ON_NOMATCH
+ *
  * @global boolean $GLOBALS['_NET_USERAGENT_MOBILE_FALLBACK_ON_NOMATCH']
  */
 $GLOBALS['_NET_USERAGENT_MOBILE_FALLBACK_ON_NOMATCH'] = false;
+
+// }}}
+// {{{ Net_UserAgent_Mobile
 
 /**
  * HTTP mobile user agent string parser
@@ -77,14 +85,30 @@ $GLOBALS['_NET_USERAGENT_MOBILE_FALLBACK_ON_NOMATCH'] = false;
  * }
  * </code>
  *
- * @package  Net_UserAgent_Mobile
- * @category Networking
- * @author   KUBO Atsuhiro <kubo@isite.co.jp>
- * @access   public
- * @version  $Revision: 1.22 $
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <kubo@isite.co.jp>
+ * @copyright  2003-2006 KUBO Atsuhiro <kubo@isite.co.jp>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @since      File available since Release 0.1
  */
 class Net_UserAgent_Mobile
 {
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
 
     /**#@+
      * @access public
@@ -112,7 +136,7 @@ class Net_UserAgent_Mobile
         static $mobileRegex;
         if (!isset($mobileRegex)) {
             $docomoRegex    = '^DoCoMo/\d\.\d[ /]';
-            $vodafoneRegex  = '^(?:(?:Vodafone|J-PHONE)/\d\.\d|MOT-)';
+            $vodafoneRegex  = '^(?:(?:SoftBank|Vodafone|J-PHONE)/\d\.\d|MOT-)';
             $ezwebRegex     = '^(?:KDDI-[A-Z]+\d+[A-Z]? )?UP\.Browser\/';
             $airhphoneRegex = '^Mozilla/3\.0\((?:DDIPOCKET|WILLCOM);';
             $mobileRegex =
@@ -141,7 +165,7 @@ class Net_UserAgent_Mobile
                                         'Net_UserAgent_Mobile_Error', true
                                         );
             }
-            if (!include_once($include)) {
+            if (!include_once $include) {
                 return PEAR::raiseError(null,
                                         NET_USERAGENT_MOBILE_ERROR_NOT_FOUND,
                                         null, null,
@@ -237,20 +261,51 @@ class Net_UserAgent_Mobile
     }
 
     /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
 }
+
+// }}}
+// {{{ Net_UserAgent_Mobile_Error
 
 /**
  * Net_UserAgent_Mobile_Error implements a class for reporting user
  * agent error messages
  *
- * @package  Net_UserAgent_Mobile
- * @category Networking
- * @author   KUBO Atsuhiro <kubo@isite.co.jp>
- * @access   public
- * @version  $Revision: 1.22 $
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <kubo@isite.co.jp>
+ * @copyright  2003-2006 KUBO Atsuhiro <kubo@isite.co.jp>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @since      Class available since Release 0.1
  */
 class Net_UserAgent_Mobile_Error extends PEAR_Error
 {
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
 
     // }}}
     // {{{ constructor
@@ -284,7 +339,19 @@ class Net_UserAgent_Mobile_Error extends PEAR_Error
                               );
         }
     }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
 }
+
+// }}}
 
 /*
  * Local Variables:
