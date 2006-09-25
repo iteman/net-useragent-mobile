0f290796b4e3d2eb40f229953a54c5f4280b113b
KUBO Atsuhiro <kubo@iteman.jp>
2006-09-25 03:05:24 +0000

    - Added support for SoftBank. - Refactored. - Changed DocBlock comments.

--- Mobile/Vodafone.php
+++ Mobile/Vodafone.php
@@ -1,26 +1,26 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: KUBO Atsuhiro <kubo@isite.co.jp>                            |
-// +----------------------------------------------------------------------+
-//
-// $Id: Vodafone.php,v 1.5 2005/08/04 05:36:19 kuboa Exp $
-//
 
-require_once(dirname(__FILE__) . '/Common.php');
-require_once(dirname(__FILE__) . '/Display.php');
+/**
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <kubo@isite.co.jp>
+ * @copyright  2003-2006 KUBO Atsuhiro <kubo@isite.co.jp>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id: Vodafone.php,v 1.6 2006/09/25 03:05:24 kuboa Exp $
+ * @since      File available since Release 0.1
+ */
+
+require_once dirname(__FILE__) . '/Common.php';
+require_once dirname(__FILE__) . '/Display.php';
 
 /**
  * Vodafone implementation
@@ -54,19 +54,27 @@ require_once(dirname(__FILE__) . '/Display.php');
  * }
  * </code>
  *
- * @package  Net_UserAgent_Mobile
- * @category Networking
- * @author   KUBO Atsuhiro <kubo@isite.co.jp>
- * @access   public
- * @version  $Revision: 1.5 $
- * @see      Net_UserAgent_Mobile_Common
- * @link     http://developers.vodafone.jp/dp/tool_dl/web/useragent.php
- * @link     http://developers.vodafone.jp/dp/tool_dl/web/position.php
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <kubo@isite.co.jp>
+ * @copyright  2003-2006 KUBO Atsuhiro <kubo@isite.co.jp>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @link       http://developers.vodafone.jp/dp/tool_dl/web/useragent.php
+ * @link       http://developers.vodafone.jp/dp/tool_dl/web/position.php
+ * @see        Net_UserAgent_Mobile_Common
+ * @since      Class available since Release 0.1
  */
 class Net_UserAgent_Mobile_Vodafone extends Net_UserAgent_Mobile_Common
 {
 
     // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
 
     /**#@+
      * @access private
@@ -163,15 +171,18 @@ class Net_UserAgent_Mobile_Vodafone extends Net_UserAgent_Mobile_Common
     function parse()
     {
         $agent = explode(' ', $this->getUserAgent());
-        preg_match('!(?:(^Vodafone)|(^J-PHONE)|(^MOT-))!', $agent[0],
-                   $matches
+        preg_match('!^(?:(SoftBank|Vodafone|J-PHONE)/\d\.\d|MOT-)!',
+                   $agent[0], $matches
                    );
-        $class = @$matches[1] ? 'Vodafone' :
-            (@$matches[2] ? 'J-PHONE' : 'Motorola');
-        $result = true;
+        if (count($matches) > 1) {
+            $carrier = $matches[1];
+        } else {
+            $carrier = 'Motorola';
+        }
 
-        switch ($class) {
+        switch ($carrier) {
         case 'Vodafone':
+        case 'SoftBank':
             $result = $this->_parseVodafone($agent);
             break;
         case 'J-PHONE':
@@ -180,14 +191,11 @@ class Net_UserAgent_Mobile_Vodafone extends Net_UserAgent_Mobile_Common
         case 'Motorola':
             $result = $this->_parseMotorola($agent);
             break;
-        default:
-            break;
         }
 
         if (Net_UserAgent_Mobile::isError($result)) {
             return $result;
         }
-
 
         $this->_msname = $this->getHeader('x-jphone-msname');
     }
@@ -530,7 +538,11 @@ class Net_UserAgent_Mobile_Vodafone extends Net_UserAgent_Mobile_Common
     }
 
     /**#@-*/
+
+    // }}}
 }
+
+// }}}
 
 /*
  * Local Variables:
