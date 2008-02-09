04f3ba674f1b7c6f2ea638112e27ab168d5aa5d0
KUBO Atsuhiro <kubo@iteman.jp>
2008-02-09 06:37:25 +0000

    - Added.

---
+++ tests/prepare.php
@@ -0,0 +1,37 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    Net_UserAgent_Mobile
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2008 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id: prepare.php,v 1.1 2008/02/09 06:37:25 kuboa Exp $
+ * @since      File available since Release 0.31.0
+ */
+
+if (file_exists(dirname(__FILE__) . '/../Mobile.php')) {
+    set_include_path(realpath(dirname(__FILE__) . '/..') . PATH_SEPARATOR .
+                     get_include_path()
+                     );
+}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
