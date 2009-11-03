23fa505012a382105f4484f055ceabf5e67f1e6e
KUBO Atsuhiro <kubo@iteman.jp>
2009-11-04 02:32:37 +0900

    Moved the product code to the src directory.

--- tests/prepare.php
+++ tests/prepare.php
@@ -39,7 +39,7 @@
 
 error_reporting(E_ALL);
 
-set_include_path(realpath(dirname(__FILE__) . '/..') . PATH_SEPARATOR .
+set_include_path(realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
                  get_include_path()
                  );
 
